733dc7ee21b6f9026e60e52e437b25aa11e3c093
Matt Schaff <matthew.schaff@gmail.com>
2018-08-08 01:33:40 -0400

    JS to make the people view look fun

--- modules/custom/neuronet_misc/js/neuronet_misc.js
+++ modules/custom/neuronet_misc/js/neuronet_misc.js
@@ -2,4 +2,20 @@
 	$('.are-you-sure').on('click', function () {
 	    return confirm('Are you sure?');
 	});
+
+	Drupal.behaviors.neuronet_thumbnails = {
+  		attach: function (context, settings) {
+	    	$.ajax({
+			  url: 'https://randomuser.me/api/?lego&results=12',
+			  dataType: 'json',
+			  success: function(data) {
+			    $('.student-thumb img').each(function(i){
+					if ($(this).attr('src').indexOf('default_images') != -1) {
+						$(this).attr('src', data.results[i]['picture']['large']);
+					}
+				});
+			  }
+			});
+		}
+	}
 })(jQuery, Drupal);
