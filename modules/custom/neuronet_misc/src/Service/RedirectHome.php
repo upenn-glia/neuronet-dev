f8dc166f03755947c0f86f3dac142451041f4c2d
Matt Schaff <mattschaff@o3world.com>
2019-09-02 16:31:21 -0400

    - Fix home & login redirects

---
+++ modules/custom/neuronet_misc/src/Service/RedirectHome.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\neuronet_misc\Service;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class RedirectHome implements EventSubscriberInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return([
+      KernelEvents::RESPONSE => [
+        ['redirectHome'],
+      ]
+    ]);
+  }
+  /**
+   * Redirect requests for the front page
+   *
+   * @param $event
+   */
+  public function redirectHome(FilterResponseEvent $event) {
+    $request = $event->getRequest();
+    if ($request->getRequestUri() === '/') {
+      $user = User::load(\Drupal::currentUser()->id());
+      if ($user->isAnonymous()) {
+        $response = new RedirectResponse("\user");
+        return $response->send();
+      }
+      if ($user->hasRole('current_student') || $user->hasRole('administrator')) {
+        $response = new RedirectResponse(\Drupal::url('view.current_students.page_1'));
+        return $response->send();
+      }
+      else {
+        $response = new RedirectResponse("\user");
+        return $response->send();
+      }
+
+    }
+  }
+}
