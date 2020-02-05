37ded4d3194860d550c3a81b4407b13d5e8aa4a4
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-02-04 21:15:42 -0500

    Add drushrc.php instead of setting $base_url

---
+++ sites/default/drushrc.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Provide the base URL so `drush user-login` can generate a correct login link
+ * @see https://x-team.com/blog/base_url-drupal-8/
+ */
+$options['uri'] = 'https://www.neuronetupenn.org';
