c9554b94af5436d713dbaeb95e06d190104f2d09
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-09-09 22:18:20 -0400

    These changes won't fix my alumni view problems; just saving them in case they're useful

--- web/modules/custom/neuronet_misc/src/Plugin/views/field/LatestJobInfo.php
+++ web/modules/custom/neuronet_misc/src/Plugin/views/field/LatestJobInfo.php
@@ -2,9 +2,14 @@
 
 namespace Drupal\neuronet_misc\Plugin\views\field;
 
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field handler for information about a profile's latest job.
@@ -16,16 +21,125 @@ use Drupal\views\ResultRow;
  */
 class LatestJobInfo extends FieldPluginBase {
 
-    // /**
-    //  * {@inheritdoc}
-    //  */
-    // protected function defineOptions() {
-    //     $options = parent::defineOptions();
+    /**
+     * The entity typemanager.
+     *
+     * @var EntityTypeManagerInterface
+     */
+    protected $entityTypeManager;
 
-    //     $options['target_field'] = ['default' => ''];
+    /**
+     * The entity field manager.
+     * 
+     * @var EntityFieldManagerInterface
+     */
+    protected $entityFieldManager;
 
-    //     return $options;
-    // }
+    /**
+     * Holds the names of job fields and whether they are entity references.
+     * 
+     * @var array
+     */
+    protected $jobFields;
+
+    /**
+     * Constructs a LatestJobInfo field plugin.
+     * 
+     * @param array $configuration
+     *  A configuration array containing information about the plugin instance.
+     * @param string $plugin_id
+     *  The plugin_id for the plugin instance.
+     * @param mixed $plugin_definition
+     *  The plugin implementation definition.
+     * @param EntityTypeManagerInterface $entity_type_manager
+     *  The entity type manager.
+     * @param EntityFieldManagerInterface $entity_field_manager
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition,
+        EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+        $this->entityTypeManager = $entity_type_manager;
+        $this->entityFieldManager = $entity_field_manager;
+
+        // Populate jobFields
+        $this->refreshJobFields();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->get('entity_type.manager'),
+            $container->get('entity_field.manager')
+        );
+    }
+
+    /**
+     * Update $jobFields to make sure we have all the latest info
+     */
+    private function refreshJobFields() {
+        $this->jobFields = [];
+
+        $allFieldDefs = $this->entityFieldManager->getFieldDefinitions('node', 'job');
+        $validNonfields = ['title', 'nid'];
+
+        foreach ($allFieldDefs as $key => $value) {
+            if (strncmp($key, 'field_', 6) == 0 || in_array($key, $validNonfields)) {
+                $this->jobFields[$key] = [
+                    'name' => $value->getLabel(),
+                    'is_ref' => $value->getType() === 'entity_reference'
+                ];
+            }
+        }
+
+        asort($this->jobFields);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function defineOptions() {
+        $options = parent::defineOptions();
+
+        $options['target_field'] = ['default' => 'title'];
+
+        # if target field is an entity reference, this controls whether to use a link to entity
+        $options['link'] = ['default' => FALSE];
+
+        return $options;
+    }
+
+    /**
+     * Create the options form where users can choose which job field to display
+     * and whether to link to content for entity reference fields.
+     */
+    public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+        parent::buildOptionsForm($form, $form_state);
+
+        // Get possible fields
+        $this->refreshJobFields();
+
+        // Make drop-down for field
+        $form['target_field'] = [
+            '#type' => 'select',
+            '#title' => $this->t('Job field to show'),
+            '#options' => array_combine(array_keys($this->jobFields), array_column($this->jobFields, 'name')),
+            '#default_value' => $this->options['target_field'],
+            '#required' => TRUE,
+        ];
+
+        $form['link'] = [
+            '#type' => 'checkbox',
+            '#title' => $this->t('Link to referenced entity'),
+            '#default_value' => $this->options['link'],
+            '#description' => $this->t('If an entity reference is selected, this controls whether to link to it.'),
+        ];
+    }
 
     /**
      * {@inheritdoc}
@@ -44,7 +158,7 @@ class LatestJobInfo extends FieldPluginBase {
         }
 
         /** @var Node[] $jobs */
-        $jobs = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([
+        $jobs = $this->entityTypeManager->getStorage('node')->loadByProperties([
             'field_profile' => $node->id(),
             'type' => 'job',
         ]);
@@ -64,8 +178,18 @@ class LatestJobInfo extends FieldPluginBase {
             }
         }
 
-        if ($latestJob && $latestJob->hasField('field_job_term')) {
-            return $latestJob->get('field_job_term');
+        $fieldname = $this->options['target_field'];
+        if ($latestJob && $latestJob->hasField($fieldname)) {
+
+            /** @var  FieldItemInterface $fieldToShow */
+            $fieldToShow = $latestJob->get($fieldname)->first();
+
+            $display_options = [];
+            if ($this->jobFields[$fieldname]['is_ref']) {
+                $display_options['settings'] = ['link' => $this->options['link']];
+            }
+
+            return $fieldToShow->view($display_options);
         } else {
             return [];
         }
